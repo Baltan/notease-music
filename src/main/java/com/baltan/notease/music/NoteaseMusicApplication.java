3eef7a3fa4cdf7578817cfcab92dbdaf77b10d01
Baltan <617640006@qq.com>
2019-12-06 22:46:34 +0800

    增加注释

--- src/main/java/com/baltan/notease/music/NoteaseMusicApplication.java
+++ src/main/java/com/baltan/notease/music/NoteaseMusicApplication.java
@@ -3,11 +3,15 @@ package com.baltan.notease.music;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Description: 项目入口启动类
+ *
+ * @author Baltan
+ * @date 2019-12-06 22:45
+ */
 @SpringBootApplication
 public class NoteaseMusicApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NoteaseMusicApplication.class, args);
     }
-
 }
