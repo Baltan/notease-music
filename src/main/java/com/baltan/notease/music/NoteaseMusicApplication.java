b614e22e6ab1fb53ee0c86571fc2f75e99525d16
Baltan <617640006@qq.com>
2019-12-06 16:35:59 +0800

    项目启动类

---
+++ src/main/java/com/baltan/notease/music/NoteaseMusicApplication.java
@@ -0,0 +1,13 @@
+package com.baltan.notease.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteaseMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteaseMusicApplication.class, args);
+    }
+
+}
