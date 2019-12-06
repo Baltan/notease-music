a4be97d1be91fcc05c6d3522c5342cda5201a073
Baltan <617640006@qq.com>
2019-12-06 16:37:39 +0800

    新增请求参数加密工具类加密测试方法

--- src/test/java/com/baltan/notease/music/NoteaseMusicApplicationTests.java
+++ src/test/java/com/baltan/notease/music/NoteaseMusicApplicationTests.java
@@ -1,11 +1,16 @@
 package com.baltan.notease.music;
 
+import com.alibaba.fastjson.JSON;
 import com.baltan.notease.music.config.NeteaseConfig;
+import com.baltan.notease.music.util.EncryptUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,5 +21,23 @@ public class NoteaseMusicApplicationTests {
     @Test
     public void testNeteaseConfig() {
         System.out.println(neteaseConfig);
+    }
+
+    @Test
+    public void testEncryptUtil() throws Exception {
+        Map<String, String> requestParams = new HashMap<>();
+        requestParams.put("hlpretag", "<span class=\\\"s-fc7\\\">");
+        requestParams.put("hlposttag", "</span>");
+        requestParams.put("#/discover", "");
+        requestParams.put("s", "李荣浩");
+        requestParams.put("type", "1");
+        requestParams.put("offset", "0");
+        requestParams.put("total", "true");
+        requestParams.put("limit", "30");
+        requestParams.put("csrf_token", "");
+        String cipherText = JSON.toJSONString(requestParams);
+        String[] paramArray = EncryptUtil.getParam(cipherText);
+        System.out.println("params: " + paramArray[0]);
+        System.out.println("encSecKey: " + paramArray[1]);
     }
 }
