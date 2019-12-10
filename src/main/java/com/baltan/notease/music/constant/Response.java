a937b71ec1940833c3fbdf46660512f13d31b9c6
Baltan <617640006@qq.com>
2019-12-10 22:52:58 +0800

    删除操作失败枚举

--- src/main/java/com/baltan/notease/music/constant/Response.java
+++ src/main/java/com/baltan/notease/music/constant/Response.java
@@ -1,13 +1,14 @@
 package com.baltan.notease.music.constant;
 
 /**
- * Description:
+ * Description: 响应成功枚举类
  *
  * @author Baltan
  * @date 2019-12-10 10:13
  */
 public enum Response {
-    SUCCESSFUL(200, "查询成功"), FAILURE(999999, "查询失败");
+    SUCCESSFUL(666666, "操作成功"),
+    NETEASE_QUERY_SUCCESSFUL(200, "网易云音乐查询成功");
 
     private final int CODE;
     private final String MESSAGE;
