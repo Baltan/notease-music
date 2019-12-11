5724fd9d9d28cfe49835ed3187e68b12995cfb5c
Baltan <617640006@qq.com>
2019-12-11 15:57:03 +0800

    新增获取MP3播放时长的方法

--- src/main/java/com/baltan/notease/music/util/PlayerUtil.java
+++ src/main/java/com/baltan/notease/music/util/PlayerUtil.java
@@ -1,15 +1,20 @@
 package com.baltan.notease.music.util;
 
 import com.baltan.notease.music.constant.CustomizedException;
+import com.baltan.notease.music.exception.DataFormatException;
 import com.baltan.notease.music.exception.MusicPlayException;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.audio.mp3.MP3AudioHeader;
+import org.jaudiotagger.audio.mp3.MP3File;
+import org.jaudiotagger.tag.TagException;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Description: 音乐播放工具类
@@ -62,6 +67,53 @@ public class PlayerUtil {
     public static void stop() throws MusicPlayException {
         try {
             player.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new MusicPlayException(CustomizedException.MUSIC_PLAY_EXCEPTION.getCODE(),
+                    CustomizedException.MUSIC_PLAY_EXCEPTION.getMESSAGE());
+        }
+    }
+
+    /**
+     * 获得MP3播放时长
+     * 参考：<a href="https://www.cnblogs.com/yansum/p/6629636.html"></a>
+     *
+     * @param file
+     * @return
+     * @throws IOException
+     * @throws CannotReadException
+     * @throws ReadOnlyFileException
+     * @throws TagException
+     * @throws InvalidAudioFrameException
+     * @throws MusicPlayException
+     */
+    public static String getDuration(File file)
+            throws IOException, CannotReadException, ReadOnlyFileException, TagException,
+            InvalidAudioFrameException, MusicPlayException, DataFormatException {
+        try {
+            MP3File f = (MP3File) AudioFileIO.read(file);
+            MP3AudioHeader audioHeader = (MP3AudioHeader) f.getAudioHeader();
+            int second = audioHeader.getTrackLength();
+            String duration = DataUtil.second2MinuteAndSecond(second);
+            return duration;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } catch (CannotReadException e) {
+            e.printStackTrace();
+            throw e;
+        } catch (ReadOnlyFileException e) {
+            e.printStackTrace();
+            throw e;
+        } catch (TagException e) {
+            e.printStackTrace();
+            throw e;
+        } catch (InvalidAudioFrameException e) {
+            e.printStackTrace();
+            throw e;
+        } catch (DataFormatException e) {
+            e.printStackTrace();
+            throw e;
         } catch (Exception e) {
             e.printStackTrace();
             throw new MusicPlayException(CustomizedException.MUSIC_PLAY_EXCEPTION.getCODE(),
