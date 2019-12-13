662a566d45c6b9f9ddc4c7cda608ad949d21275c
Baltan <617640006@qq.com>
2019-12-13 17:19:47 +0800

    重新实现播放在线音乐的方法

--- src/main/java/com/baltan/notease/music/util/PlayerUtil.java
+++ src/main/java/com/baltan/notease/music/util/PlayerUtil.java
@@ -3,10 +3,9 @@ package com.baltan.notease.music.util;
 import com.baltan.notease.music.constant.CustomizedException;
 import com.baltan.notease.music.exception.DataFormatException;
 import com.baltan.notease.music.exception.MusicPlayException;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
+import javazoom.spi.mpeg.sampled.file.MpegAudioFileReader;
 import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.exceptions.CannotReadException;
 import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
@@ -16,7 +15,9 @@ import org.jaudiotagger.audio.mp3.MP3File;
 import org.jaudiotagger.tag.TagException;
 import org.springframework.stereotype.Component;
 
+import javax.sound.sampled.*;
 import java.io.*;
+import java.net.URL;
 
 /**
  * Description: 音乐播放工具类
@@ -29,8 +30,7 @@ import java.io.*;
 @Component
 public class PlayerUtil {
     private static Player player;
-    private static Media media;
-    private static MediaPlayer mediaPlayer;
+    private static SourceDataLine sourceDataLine;
 
     /**
      * 开始本地播放
@@ -85,12 +85,66 @@ public class PlayerUtil {
      * @param url
      * @throws MusicPlayException
      */
-    public static void startOnlinePlay(String url) throws MusicPlayException {
+    public static void startOnlinePlay(URL url)
+            throws LineUnavailableException, IOException, UnsupportedAudioFileException, MusicPlayException {
         try {
-            media = new Media(url);
-            mediaPlayer = new MediaPlayer(media);
-            mediaPlayer.setAutoPlay(true);
-            mediaPlayer.play();
+            /**
+             * 获取格式为pcm的音频流
+             */
+            AudioInputStream audioInputStream = getPcmAudioInputStream(url);
+
+            if (audioInputStream == null) {
+                throw new MusicPlayException(CustomizedException.MUSIC_PLAY_EXCEPTION.getCODE(),
+                        CustomizedException.MUSIC_PLAY_EXCEPTION.getMESSAGE());
+            }
+            /**
+             * 获得此音频输入流中声音数据的音频格式
+             */
+            AudioFormat audioFormat = audioInputStream.getFormat();
+            /**
+             * 设置数据输入
+             */
+            DataLine.Info dataLineInfo =
+                    new DataLine.Info(SourceDataLine.class, audioFormat, AudioSystem.NOT_SPECIFIED);
+            /**
+             * 输出设备混频器
+             */
+            sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+            sourceDataLine.open(audioFormat);
+            sourceDataLine.start();
+            int len;
+            byte[] buf = new byte[1024];
+            /**
+             * 从音频输入流中读取数据
+             */
+            while ((len = audioInputStream.read(buf)) > 0) {
+                /**
+                 * 将数据写入混音器
+                 */
+                sourceDataLine.write(buf, 0, len);
+            }
+            /**
+             * 阻塞等待，清空缓冲数据
+             */
+            sourceDataLine.drain();
+            /**
+             * 关闭音频流
+             */
+            audioInputStream.close();
+            /**
+             * 停止播放
+             */
+            sourceDataLine.stop();
+            sourceDataLine.close();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+            throw e;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+            throw e;
         } catch (Exception e) {
             e.printStackTrace();
             throw new MusicPlayException(CustomizedException.MUSIC_PLAY_EXCEPTION.getCODE(),
@@ -105,8 +159,8 @@ public class PlayerUtil {
      */
     public static void stopOnlinePlay() throws MusicPlayException {
         try {
-            mediaPlayer.stop();
-            mediaPlayer.dispose();
+            sourceDataLine.stop();
+            sourceDataLine.close();
         } catch (Exception e) {
             e.printStackTrace();
             throw new MusicPlayException(CustomizedException.MUSIC_PLAY_EXCEPTION.getCODE(),
@@ -115,13 +169,48 @@ public class PlayerUtil {
     }
 
     /**
-     * 暂停在线播放
+     * 获取音源文件的音频流
      *
+     * @param url
+     * @return
+     * @throws UnsupportedAudioFileException
+     * @throws IOException
      * @throws MusicPlayException
      */
-    public static void pauseOnlinePlay() throws MusicPlayException {
+    private static AudioInputStream getPcmAudioInputStream(URL url)
+            throws UnsupportedAudioFileException, IOException, MusicPlayException {
         try {
-            mediaPlayer.pause();
+            /**
+             * 读取音频文件的类
+             */
+            MpegAudioFileReader reader = new MpegAudioFileReader();
+            /**
+             * 获取音频输入流，从提供的音源文件获得音频输入流，也支持从本地文件读取：mp.getAudioInputStream(file)
+             */
+            AudioInputStream sourceInputStream = reader.getAudioInputStream(url);
+            /**
+             * 获得此音频输入流中声音数据的音频格式
+             */
+            AudioFormat audioFormat = sourceInputStream.getFormat();
+            /**
+             * 设定输出格式为pcm格式的音频文件
+             */
+            AudioFormat format =
+                    new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, audioFormat.getSampleRate(), 16,
+                            audioFormat.getChannels(), audioFormat.getChannels() * 2,
+                            audioFormat.getSampleRate(),
+                            false);
+            /**
+             * 输出到音频
+             */
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(format, sourceInputStream);
+            return audioInputStream;
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+            throw e;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
         } catch (Exception e) {
             e.printStackTrace();
             throw new MusicPlayException(CustomizedException.MUSIC_PLAY_EXCEPTION.getCODE(),
